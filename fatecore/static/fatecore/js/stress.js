4d606d77ed2cf7767a58c7af9797464f312cf39b
cthos <daginus@gmail.com>
2014-04-13 19:02:49 -0400

    Quick and dirty consequences track.

--- fatecore/static/fatecore/js/stress.js
+++ fatecore/static/fatecore/js/stress.js
@@ -1,11 +1,17 @@
 (function ($) {
   $(function () {
-    $('.stress-box').click(function () {
+    $('.checkable').click(function () {
       $(this).toggleClass('checked');
     });
 
     $('.reset-stress-button').on('click', 'a', function() {
       $('.stress-box.checked').removeClass('checked');
+    });
+
+    $('.consequences .checkable').click(function () {
+      var tar = $(this).data('target');
+
+      $('.' + tar).toggle('fast');
     });
   });
 }) ($);
